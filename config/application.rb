498b3a95226c629df8b9077956160b0ad5ba3f4a
Sholom <boruchsteinmetz@outlook.com>
2018-05-01 22:09:41 -0400

    modified rack-cors to allow all urls

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module MtlAnashApi2
     
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:3000'
+        origins '*'
         resource '*', :headers => :any, :methods => [:get]
       end
     end
