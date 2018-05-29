9fa2ce1cd26fbfff5cc09ac7c4b8bbc90ea8cff4
Sholom <boruchsteinmetz@outlook.com>
2018-05-29 11:54:34 -0400

    add session#method authenticates user and redirects to people page

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -3,10 +3,16 @@ class SessionController < ApplicationController
   end
 
   def new
-    @user = User.new 
+
   end
 
   def create
+    if !User.find_by_name(name_param).try(:authenticate, password_param )
+      puts password_param
+      redirect_to new_session_path
+    else
+      redirect_to '/people' 
+    end
   end
 
   def logout
@@ -14,4 +20,21 @@ class SessionController < ApplicationController
 
   def destroy
   end
+
+  private 
+  
+  def new_session_params
+    params.permit(:name, :password )
+  end
+
+  def name_param
+    params.permit(:name)['name']
+  end
+
+  def password_param
+    params.permit(:password )['password']
+  end
+
+  
+
 end
