10a40ded554e7fa3c163b69440366c092d5e0268
Sholom <boruchsteinmetz@outlook.com>
2018-07-03 22:07:47 -0400

    modify people#new loader class

--- app/assets/javascripts/people_form.js
+++ app/assets/javascripts/people_form.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
     formNode.addEventListener('submit', (element) => {
         element.target.style.display = 'none'
         const uploadDiv = document.getElementById('upload-loader')
-        uploadDiv.innerHTML = "<div class='loader'> </div> <h1>Hang on Tight!</h1>"
+        uploadDiv.innerHTML = "<div class='people-loader'> </div> <h1>Hang on Tight!</h1>"
     })
 
     
